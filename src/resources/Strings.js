2c1856d68b0bcbaba6571f4211a7ea7534f8f0c8
hamyarbaiat <Untouchable.rb@gmail.com>
2019-04-22 16:48:38 +0330

    [TIPI-14] deployment bug fixed

--- src/resources/Strings.js
+++ src/resources/Strings.js
@@ -1,6 +1,20 @@
 /* eslint-disable */
 import EN from '../../assets/translations/EN';
 import FA from '../../assets/translations/FA';
+import i18next from 'i18next';
+
+i18next.init({
+    lng: 'en',
+    debug: true,
+    resources: {
+        en: {
+            translation: EN
+        }
+    }
+}, function(err, t) {
+    // initialized and ready to go!
+    document.getElementById('output').innerHTML = i18next.t('key');
+});
 
 export default class Strings {
 
